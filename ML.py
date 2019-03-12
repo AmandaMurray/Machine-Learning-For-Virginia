8227d48db4131f1e7c1be7c5b5c30c74cdf1093d
AmandaMurray <36601078+AmandaMurray@users.noreply.github.com>
2019-03-12 17:23:39 -0400

    Create ML.py
    Wheeee!

---
+++ ML.py
@@ -0,0 +1,55 @@
+#Hey look we have an ML Document where we can maybe probably understand WTF is happening :)
+
+#Because we need all these ALL THE FUCKING TIME
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+
+#For data cleaning+Processing+Discovery
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import Imputer
+
+#For Cross-Evaluation when tuning hyperparameter
+from sklearn.model_selection import cross_val_score
+
+#For SVMs
+from sklearn.svm import LinearSVC #We probably won't have linear data
+from sklearn.svm import SVC
+
+#For evaluating a classfication algorithm
+from sklearn.metrics import precision_score, recall_score, accuracy_score #These take .predict !!!Use np.ravel(y_test) rather than y_test
+from sklearn.metrics import roc_curve #Remember to send the SVC.decision_function to this rather than the .predict
+
+
+## Import, clean, prepare the data, look at the correlations
+    ## This is the point we're we'd be creating the "Busy-ness" feature
+
+
+##Classification algorithm
+    ##Try a linear SVC for a few C
+
+
+    ##Kernelize the SVC. We don't know how these work, but we'll try a few different ones and pick the one with the best performance
+
+    ##Optimize whichever performed the best above using cross-evaluation
+
+
+    ##Display and understand the data
+
+
+##Logistics Regression Algorithm
+    ##We'll have to do research for optimizing and changing forms on this, but it may just be adjusting the regularization hyperparameter
+
+
+## Clustering Algorithm
+    ## We know nothing about this yet. Hopefully we'll learn it before we have to do it
+
+
+##Decision trees?
+    ##I think decisions trees might be a valid approach to this problem?
